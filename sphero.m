4e71bf71887bae5a8369d46fab18d1e757111128
Joshua Fagan <jfagan2@vols.utk.edu>
2020-05-12 13:25:58 -0400

    Adds desctructor function to gracefully close everything when cleared

--- sphero.m
+++ sphero.m
@@ -67,6 +67,23 @@ classdef sphero < handle
             this.init_topics();
             this.init_pubs_and_subs();
         end % Coonnect method
+        
+        function delete(this)
+            % Class destructor, closes connection to Pi and terminates all running processes
+            
+            % Remove unneeded output file from Pi
+            system( this.mypi, 'rm ~/launch_ros_nodes_output.txt -f' );
+            
+            % Restart Pi, terminating all processes being run on Pi
+            try
+                system( this.mypi, 'sudo shutdown -r now' );
+            catch E
+            end
+            clear this.mypi;
+            
+            % Shutdown ROS master
+            rosshutdown;
+        end % Delete method
     end % Public methods
     
     methods (Access=private)
