b5df1b95ecb23de4628b392dfd4bdbda7206a460
Joshua Fagan <jfagan2@vols.utk.edu>
2020-06-04 09:28:48 -0400

    Configures paths

---
+++ configure_installation.m
@@ -0,0 +1,11 @@
+% Add current directory to the path and save it
+path(path, pwd);
+path(path, genpath(sprintf( '%s/msg_gen/matlab_gen', pwd )))
+savepath;
+
+% Add Matlab generated message files to the static java class path
+[class_dir, ~, ~] = fileparts(mfilename('fullpath'));
+cmd = sprintf('echo %s/msg_gen/matlab_gen/jar/drive_control-0.0.0.jar >> ''%s/javaclasspath.txt''', class_dir, prefdir);
+system(cmd);
+
+disp('Please restart Matlab for configuration to take effect.');
