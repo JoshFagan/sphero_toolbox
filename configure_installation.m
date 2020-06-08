faeba89733abcc5ea664f69d8df677febab5421a
Joshua Fagan <jfagan2@vols.utk.edu>
2020-06-08 13:41:30 -0400

    Adapts to change in action name

--- configure_installation.m
+++ configure_installation.m
@@ -5,7 +5,7 @@ savepath;
 
 % Add Matlab generated message files to the static java class path
 [class_dir, ~, ~] = fileparts(mfilename('fullpath'));
-cmd = sprintf('echo %s/msg_gen/matlab_gen/jar/drive_control-0.0.0.jar >> ''%s/javaclasspath.txt''', class_dir, prefdir);
+cmd = sprintf('echo %s/msg_gen/matlab_gen/jar/sphero_msgs-0.0.0.jar >> ''%s/javaclasspath.txt''', class_dir, prefdir);
 system(cmd);
 
 disp('Please restart Matlab for configuration to take effect.');
