423d1e2d9964768b1e4508681ee491d86fea5e50
Joshua Fagan <jfagan2@vols.utk.edu>
2020-07-27 15:32:58 -0500

    Creates and sends IMU messages

--- catkin_ws/src/sensor_control/src/publish_sphero_sensors.py
+++ catkin_ws/src/sensor_control/src/publish_sphero_sensors.py
@@ -13,6 +13,8 @@ from sphero_sdk import SpheroRvrObserver
 from sphero_sdk import RvrStreamingServices
 
 from std_msgs.msg import ColorRGBA, Float64
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Quaternion, Vector3
 
 
 class SensorPublisher():
@@ -24,6 +26,8 @@ class SensorPublisher():
                                          ColorRGBA, queue_size=1)
         self.amb_light_pub = rospy.Publisher('/sphero_sensors/ambient_light',
                                          Float64, queue_size=1)
+        self.imu_pub = rospy.Publisher('/sphero_sensors/imu',
+                                         Imu, queue_size=1)
 
         # Color detection
         self.rvr.enable_color_detection(is_enabled=True)
@@ -36,21 +40,21 @@ class SensorPublisher():
             service=RvrStreamingServices.ambient_light,
             handler=self.ambient_light_handler
         )
-#        # IMU
-#        self.rvr.sensor_control.add_sensor_data_handler(
-#            service=RvrStreamingServices.imu,
-#            handler=self.imu_handler
-#        )
-#        # Accelerometer
-#        self.rvr.sensor_control.add_sensor_data_handler(
-#            service=RvrStreamingServices.accelerometer,
-#            handler=self.accelerometer_handler
-#        )
-#        # Gyroscope
-#        self.rvr.sensor_control.add_sensor_data_handler(
-#            service=RvrStreamingServices.gyroscope,
-#            handler=self.gyroscope_handler
-#        )
+        # IMU
+        self.rvr.sensor_control.add_sensor_data_handler(
+            service=RvrStreamingServices.imu,
+            handler=self.imu_handler
+        )
+        # Accelerometer
+        self.rvr.sensor_control.add_sensor_data_handler(
+            service=RvrStreamingServices.accelerometer,
+            handler=self.empty_handler
+        )
+        # Gyroscope
+        self.rvr.sensor_control.add_sensor_data_handler(
+            service=RvrStreamingServices.gyroscope,
+            handler=self.empty_handler
+        )
 #        # Locator
 #        self.rvr.sensor_control.add_sensor_data_handler(
 #            service=RvrStreamingServices.locator,
@@ -67,7 +71,7 @@ class SensorPublisher():
 #            handler=self.speed_handler
 #        )
 
-        self.rvr.sensor_control.start(interval=1000)
+        self.rvr.sensor_control.start(interval=10000)
 
 
     def color_detected_handler(self, color_data):
@@ -84,15 +88,27 @@ class SensorPublisher():
     
     
     def imu_handler(self, imu_data):
-        print('IMU data response: ', imu_data)
+        orientation = Quaternion(imu_data['IMU']['Roll'],
+                                 imu_data['IMU']['Pitch'],
+                                 imu_data['IMU']['Yaw'],
+                                 0)
+
+        angular_velocity = Vector3(imu_data['Gyroscope']['X'],
+                                   imu_data['Gyroscope']['Y'],
+                                   imu_data['Gyroscope']['Z'])
+
+        linear_acceleration = Vector3(imu_data['Accelerometer']['X'],
+                                      imu_data['Accelerometer']['Y'], 
+                                      imu_data['Accelerometer']['Z'])
+
+        imu_msg = Imu(orientation=orientation, 
+                      angular_velocity=angular_velocity, 
+                      linear_acceleration=linear_acceleration)
+        self.imu_pub.publish(imu_msg)
     
     
-    def accelerometer_handler(self, accelerometer_data):
-        print('Accelerometer data response: ', accelerometer_data)
-    
-    
-    def gyroscope_handler(self, gyroscope_data):
-        print('Gyroscope data response: ', gyroscope_data)
+    def empty_handler(self, accelerometer_data):
+        pass
     
     
     def locator_handler(self, locator_data):
@@ -111,6 +127,8 @@ if __name__ == '__main__':
         rospy.init_node('sensor_publisher')
     
         rvr = SpheroRvrObserver()
+        print( rvr.sensor_control.supported_sensors())
+        print( rvr.sensor_control.enabled_sensors())
         rvr.wake()
 
         # Give RVR time to wake up
