53e4553543bcb00ad38e13f7f9158df433b0a649
Joshua Fagan <jfagan2@vols.utk.edu>
2020-07-28 14:07:47 -0400

    Structures values returned from sensor topic

--- @sphero/getDetectedColor.m
+++ @sphero/getDetectedColor.m
@@ -1,7 +1,11 @@
-function getDetectedColor()
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
-    message_received = receive(this.color_detect_sub).Data; % Wait for responce from Sphero
-    disp(message_received)
+function [color,confidence] = getDetectedColor()
+% GETDETECTEDCOLOR retrieves the latest color detected onboard the Sphero RVR
+% color = GETDETECTEDCOLOR() retrieves the latest color detected
+% color, confidence = GETDETECTEDCOLOR() retrieves the latests color
+% detected and the confidence in that color being correctly classified
+
+    msg = receive(this.color_detect_sub); % Wait for responce from Sphero
+    color = [msg.R, msg.G, msg.B];
+    confidence = msg.A;
 end
 
