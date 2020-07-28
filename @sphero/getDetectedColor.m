95ad7b5666aa4fc0083c4b950047ff66ad697217
Joshua Fagan <jfagan2@vols.utk.edu>
2020-07-28 16:36:50 -0400

    Changes output to structure

--- @sphero/getDetectedColor.m
+++ @sphero/getDetectedColor.m
@@ -1,11 +1,11 @@
-function [color, confidence] = getDetectedColor(this)
+function [color] = getDetectedColor(this)
 % GETDETECTEDCOLOR  Retrieves the latest color detected onboard the Sphero RVR
 %   color = GETDETECTEDCOLOR() retrieves the latest color detected
 %   color, confidence = GETDETECTEDCOLOR() retrieves the latests color
 %   detected and the confidence in that color being correctly classified
 
     msg = receive(this.color_detect_sub); % Wait for responce from Sphero
-    color = [msg.R, msg.G, msg.B];
-    confidence = msg.A;
+    color.R = msg.R; color.G = msg.G; color.B = msg.B;
+    color.confidence = msg.A;
 end
 
