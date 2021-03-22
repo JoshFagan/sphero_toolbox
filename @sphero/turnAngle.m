e6ec36153ec9fc77b2fa089244aeb77ae324015a
Joshua Fagan <jfagan2@vols.utk.edu>
2021-03-22 14:09:47 -0400

    Change variable names for clarity

--- @sphero/turnAngle.m
+++ @sphero/turnAngle.m
@@ -1,9 +1,9 @@
-function turnAngle(this, degrees)
+function turnAngle(this, angle)
     % Turn degrees specified
     % Positive degrees turns clockwise, negaive degrees turns counter-clockwise
     
     this.drive_control_m.Command = 'turn_angle';
-    this.drive_control_m.Degrees = degrees;
+    this.drive_control_m.Angle = angle;
 
     sendGoal(this.drive_control_ac, this.drive_control_m);
 end % Set drive velocity function
