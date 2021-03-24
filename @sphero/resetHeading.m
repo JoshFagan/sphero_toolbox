2c04ef03864bce10c36adc0842f0f35f3714a73d
Joshua Fagan <jfagan2@vols.utk.edu>
2021-03-24 13:39:47 -0400

    Update documentation

--- @sphero/resetHeading.m
+++ @sphero/resetHeading.m
@@ -1,5 +1,16 @@
 function resetHeading(this)
-% RESETHEADING Reset the heading of the RVR
+% RESETHEADING Reset the robots idea of "forward" to current direction
+% 
+%   The turnAngle function automatically does this. This is only needed if
+%   the client is keeping and using an up-to-date estimation of the robots
+%   heading.
+%   Input:
+%       none
+%   Output:
+%       none
+%   Examples:
+%       s.resetHeading() % Stops robot's movement
+
     this.drive_control_m.Command    = 'reset_heading';
     
     sendGoal(this.drive_control_ac, this.drive_control_m);
