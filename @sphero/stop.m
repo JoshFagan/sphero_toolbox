7ed21dbffa8f28e6d1e51d34c94569ff9cc48a0c
Joshua Fagan <jfagan2@vols.utk.edu>
2021-03-24 12:25:13 -0400

    Update documentation

--- @sphero/stop.m
+++ @sphero/stop.m
@@ -1,7 +1,13 @@
 function stop(this)
-    % Stop robot motors
+% STOP Stop robot's forward or angular movement.
+%   Input:
+%       none
+%   Output:
+%       none
+%   Examples:
+%       s.stop() % Stops robot's movement
 
     this.drive_control_m.Command = 'stop';
 
     sendGoal(this.drive_control_ac, this.drive_control_m);
-end % Set drive velocity function
+end % Stop function
