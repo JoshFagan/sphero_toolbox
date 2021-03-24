883d0a0417b8f04c587ac3bfb026623e34233b77
Joshua Fagan <jfagan2@vols.utk.edu>
2021-03-24 13:56:22 -0400

    Update documentation

--- @sphero/getSpeed.m
+++ @sphero/getSpeed.m
@@ -1,6 +1,12 @@
 function [speed] = getSpeed(this)
 % GETSPEED  Retrieves the latest speed reading detected onboard the Sphero RVR
-%   speed = GETSPEED() latest speed reading
+% onboard the Sphero RVR.
+%   Input:
+%       none
+%   Output:
+%       speed: A single value indicating the current speed of the robot
+%   Examples:
+%       speed = s.getSpeed()
 
     msg = receive(this.speed_sub); % Wait for responce from Sphero
     speed = msg.Data;
