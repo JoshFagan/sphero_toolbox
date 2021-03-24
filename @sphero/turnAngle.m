28a244c860faa9d31786145928b69058d3673bb8
Joshua Fagan <jfagan2@vols.utk.edu>
2021-03-24 12:21:40 -0400

    Update documentation and input validation

--- @sphero/turnAngle.m
+++ @sphero/turnAngle.m
@@ -1,7 +1,24 @@
 function turnAngle(this, angle)
-    % Turn degrees specified
-    % Positive degrees turns clockwise, negaive degrees turns counter-clockwise
-    
+% TURNANGLE Turn robot in place to angle specified in degrees.
+%   Robot will turn in whichever direction will result in the shortest
+%   path to the desired angle.
+%   Input:
+%       angle: (deg)
+%   Output:
+%       none
+%   Examples:
+%       s.turnAngle(-180) % rotate half turn clockwise
+
+    if nargin ~= 2
+        warning('Angle required to turn');
+        return
+    end
+
+    if ~isnumeric(angle)
+        warning('Angle must be a numeric value')
+        return
+    end
+
     this.drive_control_m.Command = 'turn_angle';
     this.drive_control_m.Angle = angle;
 
