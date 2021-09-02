57cf497b834d2bb0a232c8fe9496b7df650b6f48
Joshua Fagan <jfagan2@vols.utk.edu>
2021-09-02 10:03:50 -0400

    Updates configuration for use with R2021b

--- configure_installation.m
+++ configure_installation.m
@@ -1,19 +1,24 @@
 % Add Matlab generated message files to the static java class path
-[class_dir, ~, ~] = fileparts(mfilename('fullpath'));
+%[class_dir, ~, ~] = fileparts(mfilename('fullpath'));
 
 if ismac || isunix
-    msg_path = sprintf( '%s/msg_gen/matlab_gen', pwd );
-    cmd = sprintf('echo %s/msg_gen/matlab_gen/jar/sphero_msgs-0.0.0.jar >> ''%s/javaclasspath.txt''', class_dir, prefdir);
+    msg_path = sprintf( '%s/custom_msgs/matlab_msg_gen_ros1/glnxa64/install/m', pwd );
+    %msg_path = sprintf( '%s/msg_gen/matlab_gen', pwd );
+    %cmd = sprintf('echo %s/msg_gen/matlab_gen/jar/sphero_msgs-0.0.0.jar >> ''%s/javaclasspath.txt''', class_dir, prefdir);
 elseif ispc
-    msg_path = sprintf( '%s\\msg_gen\\matlab_gen', pwd );
-    cmd = sprintf('echo %s\\msg_gen\\matlab_gen\\jar\\sphero_msgs-0.0.0.jar >> ""%s/javaclasspath.txt""', class_dir, prefdir);
+    msg_path = sprintf( '%s\\custom_msgs\\matlab_msg_gen_ros1\\glnxa64\\install\\m', pwd );
+    %msg_path = sprintf( '%s\\msg_gen\\matlab_gen', pwd );
+    %cmd = sprintf('echo %s\\msg_gen\\matlab_gen\\jar\\sphero_msgs-0.0.0.jar >> ""%s/javaclasspath.txt""', class_dir, prefdir);
 end
 
-system(cmd);
+%system(cmd);
 
 % Add current directory to the path and save it
-path(path, pwd);
-path(path, msg_path);
+%path(path, pwd);
+%path(path, msg_path);
+addpath(msg_path);
 savepath;
+clear classes;
+rehash toolboxcache;
 
-disp('Please restart Matlab for configuration to take effect.');
+%disp('Please restart Matlab for configuration to take effect.');
