0514196b83d93a51f6ef2cc0108e042176b54e4d
Joshua Fagan <jfagan2@vols.utk.edu>
2021-09-24 10:56:36 -0400

    Uses ROS messages generated on Mac

--- custom_msgs/matlab_msg_gen_ros1/glnxa64/devel/share/sphero_msgs/cmake/sphero_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(sphero_msgs_MSG_INCLUDE_DIRS "/home/josh/GTA/sphero_toolbox/custom_msgs/matlab_msg_gen_ros1/glnxa64/devel/share/sphero_msgs/msg")
-set(sphero_msgs_MSG_DEPENDENCIES std_msgs;actionlib_msgs)
