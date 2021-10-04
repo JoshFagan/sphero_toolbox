c21af158144360e98697fc9b9024b5feae2e49b7
Joshua Fagan <jfagan2@vols.utk.edu>
2021-10-04 12:18:59 -0400

    Adapt instructoins to generate messages with R2021b

--- configure_installation.m
+++ configure_installation.m
@@ -1,15 +1,19 @@
-% Add Matlab generated message files to the path
-if ismac || isunix
-    msg_path = sprintf( '%s/custom_msgs/matlab_msg_gen_ros1/glnxa64/install/m', pwd );
+% Generate custom ROS message files
+rosgenmsg('custom_msgs', 'BuildConfiguration', 'fasterruns');
+
+% Add current directory and custom message paths to the search path and save it
+if ismac
+    msg_path = sprintf( '%s/custom_msgs/matlab_msg_gen_ros1/maci64/install/m', pwd );
 elseif ispc
-    msg_path = sprintf( '%s\\custom_msgs\\matlab_msg_gen_ros1\\glnxa64\\install\\m', pwd );
+    msg_path = sprintf( '%s\\custom_msgs\\matlab_msg_gen_ros1\\maci64\\install\\m', pwd );
+else
+    msg_path = sprintf( '%s/custom_msgs/matlab_msg_gen_ros1/glnxa64/install/m', pwd );
 end
 
-%system(cmd);
-
-% Add current directory to the path and save it
 addpath(pwd);
 addpath(msg_path);
 savepath;
+
+% Perfrom custom message refresh
 clear classes;
 rehash toolboxcache;
