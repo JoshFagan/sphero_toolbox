d1abc2b0684453b9fd2c45a51119fabb0fbffb24
Joshua Fagan <jfagan2@vols.utk.edu>
2022-01-20 13:47:25 -0500

    Kills node on Pi before shutting down ros

--- @sphero/delete.m
+++ @sphero/delete.m
@@ -1,12 +1,15 @@
 function delete(this)
     % Class destructor, closes connection to Pi and terminates all running processes
+    system( this.mypi, 'rosnode kill /rvr_control');
+    pause(1)
     % Shutdown ROS master
     rosshutdown;
     
     % Remove unneeded output file from Pi
     if ~isempty(this.mypi)
-        system( this.mypi, 'rm -f ~/launch_ros_nodes_output.txt' );
-    
+        system( this.mypi, 'rm -f ~/.ros/log/*.log');
+        system( this.mypi, 'rm -rf ~/.ros/log/*' );
+
         % Restart Pi, terminating all processes being run on Pi
         try
             system( this.mypi, 'sudo reboot +0.2' );
