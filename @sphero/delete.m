5c90a8f6a06122e28db8401d36c156b88b26cf47
Joshua Fagan <jfagan2@vols.utk.edu>
2022-01-20 12:17:13 -0500

    Modified commands to make shutting down more graceful

--- @sphero/delete.m
+++ @sphero/delete.m
@@ -1,18 +1,17 @@
 function delete(this)
     % Class destructor, closes connection to Pi and terminates all running processes
+    % Shutdown ROS master
+    rosshutdown;
     
     % Remove unneeded output file from Pi
     if ~isempty(this.mypi)
-        system( this.mypi, 'rm ~/launch_ros_nodes_output.txt -f' );
+        system( this.mypi, 'rm -f ~/launch_ros_nodes_output.txt' );
     
         % Restart Pi, terminating all processes being run on Pi
         try
-            system( this.mypi, 'sudo shutdown -r now' );
+            system( this.mypi, 'sudo reboot +0.2' );
         catch E
         end
         clear this.mypi;
     end
-    
-    % Shutdown ROS master
-    rosshutdown;
 end % Delete method
