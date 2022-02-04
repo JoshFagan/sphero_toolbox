a141b6d1c064d1115915bb07536e270dab2f3563
JoshFagan <jfagan2@vols.utk.edu>
2022-02-04 10:17:26 -0500

    Updates config to not use custom messages

--- configure_installation.m
+++ configure_installation.m
@@ -1,2 +1,6 @@
-% Generate custom ROS message files
-rosgenmsg('custom_msgs', 'BuildConfiguration', 'fasterruns');
+% Add sphero_toolbox to path and save path
+addpath('.');
+savepath;
+
+% Set python environemnt path
+pyenv('Version','2.7');
