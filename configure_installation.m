4f8004926a2c2bb6f3211e2dea645fb5a57370fe
JoshFagan <jfagan2@vols.utk.edu>
2022-02-14 17:18:22 -0500

    Makes updating python venv variable persistant

--- configure_installation.m
+++ configure_installation.m
@@ -4,6 +4,16 @@ savepath;
 
 % Set python environemnt path
 pyenv('Version','2.7');
-if ispc
-    setenv('MY_PYTHON2_VENV', 'C:\Matlab_Python_Env') 
+% Add startup if on pc and space in userpath
+if ispc && any(isspace(userpath))
+    % Open startup.m file in default Documents/MATLAB
+    fileID = fopen(fullfile(userpath,'startup.m'),'w');
+
+    % Print setenv to make change persistent across MATLAB sessions
+    envtext = "setenv('MY_PYTHON2_VENV', 'C:\Matlab_Python_Env')";
+    fprintf(fileID,'%s',envtext);
+
+    % Close file and run setenv so user doesn't need to restart
+    fclose(fileID);
+    setenv('MY_PYTHON2_VENV', 'C:\Matlab_Python_Env')
 end
