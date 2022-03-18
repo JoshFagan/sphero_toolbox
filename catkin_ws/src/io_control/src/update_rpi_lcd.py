2e7c1f29945690e458a56f50c67a0aaf462c5172
abiegals@utk.edu <abiegals@utk.edu>
2022-03-18 10:34:43 -0500

    Adds package for setting RPi IO devices

---
+++ catkin_ws/src/io_control/src/update_rpi_lcd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import qwiic
+
+import rospy
+import roslib
+roslib.load_manifest('io_control')
+from sphero_msgs.msg import OledInput 
+
+
+class UpdateRpiOLED():
+    def __init__(self):
+        self.oled = qwiic.QwiicMicroOled()
+        self.oled.begin()
+        self.oled.clear(oled.ALL)
+
+        # Subscribers
+        rospy.Subscriber("/rpi_io/oled_input", 
+                         OledInput, self.request_handler)
+
+
+    def request_handler(self, request):
+        self.oled.clear(oled.PAGE)
+        self.oled.set_cursor(request.row, request.col)
+        self.oled.print(reqest.text)
+        self.oled.display()
+
+
+    def __del__(self):
+        self.oled.clear(oled.PAGE)
+        self.oled.display()
+    
+    
+if __name__ == '__main__':
+    try:
+        rospy.init_node('update_rpi_oled')
+    
+        oled_sub = UpdateRpiOLED()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
